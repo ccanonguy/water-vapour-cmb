7a8d53d5a818c199b0f960011b86fdd80082c74c
ccanonguy <singhgurbir10@gmail.com>
2017-05-17 15:55:45 +0900

    added plots

--- am/makeMulPlots.py
+++ am/makeMulPlots.py
@@ -5,12 +5,17 @@ import sys
 
 pwd = "/home/gurbir/kek/water-vapour-cmb/am/"
 def pwv2trop(pwv):
-	return (float(pwv)-0.0029)/1.85
+	return (float(pwv)-0.0029)/1.8502
 
 plt.figure()
 filename = sys.argv[1]
 for j in range(int(sys.argv[2])):
-	pwv = sys.argv[3+j]
+	pwv1 = sys.argv[3+j]
+	pwv = 0
+	if pwv1 != '0':
+		pwv = pwv1
+	else:
+		pwv = 0.003
 	trop_h20_scale = pwv2trop(pwv)
 	call("am " + pwd + filename + " 0 GHz 200 GHz 10 MHz 0 deg " +  str(trop_h20_scale) + " > out"+str(j), shell=True)
 	f = open('out'+str(j), 'r')
@@ -21,7 +26,11 @@ for j in range(int(sys.argv[2])):
 		x.append(words[0])
 		y.append(words[1])
 
-	plt.plot(x, y)
+	plt.plot(x, y, label=pwv1 + " mm")
 	# plt.xlim(15,30)
-
+plt.legend(loc='upper right', bbox_to_anchor=(0.95,0.6))
+plt.xlabel("Frequency (GHz)")
+plt.ylabel("Tb (K)")
+plt.title("Radiation temperature vs Frequency for different PWV")
+plt.savefig('Tb_vs_freq_0_5_10_20_50.png')
 plt.show()
--- am/makePlot.py
+++ am/makePlot.py
@@ -5,9 +5,14 @@ import sys
 
 pwd = "/home/gurbir/kek/water-vapour-cmb/am/"
 def pwv2trop(pwv):
-	return (float(pwv)-0.0029)/1.85
+	return (float(pwv)-0.0029)/1.8502
 
-pwv = sys.argv[2]
+pwv1 = sys.argv[2]
+pwv = 0
+if pwv1 != '0':
+	pwv = pwv1
+else:
+	pwv = 0.003
 filename = sys.argv[1]
 trop_h20_scale = pwv2trop(pwv)
 call("am " + pwd + filename + " 0 GHz 200 GHz 10 MHz 0 deg " +  str(trop_h20_scale) + " > out", shell=True)
@@ -21,5 +26,9 @@ for line in f:
 
 plt.figure()
 plt.plot(x, y)
+plt.xlabel("Frequency (GHz)")
+plt.ylabel("Tb (K)")
+plt.title("Radiation temperature vs Frequency for " + pwv1 + " mm PWV")
+plt.savefig('Tb_vs_freq_' + pwv1 + '.png')
 # plt.xlim(15,30)
 plt.show()
