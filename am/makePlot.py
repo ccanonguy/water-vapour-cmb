343177a0dd2d414ca8d6779088aefdf27b6c9de7
ccanonguy <singhgurbir10@gmail.com>
2017-05-17 14:48:01 +0900

    modelled the 0-200 GHz spectrum

--- am/makePlot.py
+++ am/makePlot.py
@@ -1,14 +1,25 @@
+from subprocess import call
 import matplotlib.pyplot as plt
+import sys
 
 
-f = open('example2.5.0-200.out', 'r')
+pwd = "/home/gurbir/kek/water-vapour-cmb/am/"
+def pwv2trop(pwv):
+	return (float(pwv)-0.0029)/1.85
+
+pwv = sys.argv[2]
+filename = sys.argv[1]
+trop_h20_scale = pwv2trop(pwv)
+call("am " + pwd + filename + " 0 GHz 200 GHz 10 MHz 0 deg " +  str(trop_h20_scale) + " > out", shell=True)
+f = open('out', 'r')
 x = []
 y = []
 for line in f:
 	words = line.split(' ')
 	x.append(words[0])
-	y.append(words[2])
+	y.append(words[1])
 
 plt.figure()
 plt.plot(x, y)
+# plt.xlim(15,30)
 plt.show()
