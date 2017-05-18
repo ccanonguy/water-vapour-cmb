546408ca8de78804165e8a678427e569c56810c6
ccanonguy <singhgurbir10@gmail.com>
2017-05-18 17:45:30 +0900

    fitting and plotted

--- analysis.py
+++ analysis.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import csv
 from pprint import pprint
 import numpy as np
+from lmfit import minimize, Minimizer, Parameters, Parameter, report_fit
+from subprocess import call
 
 
 power = []
@@ -83,6 +85,18 @@ for k in range(3):
 	dum = [np.std(tsky[k][j:j+40]) for j in range(0, i, 40)]
 	sd.append(dum)
 
+xvalueHist = xvalueHist[8:]
+tskyHist[0] = tskyHist[0][8:]
+tskyHist[1] = tskyHist[1][8:]
+tskyHist[2] = tskyHist[2][8:]
+sd[0] = sd[0][8:]
+sd[1] = sd[1][8:]
+sd[2] = sd[2][8:]
+
+np.asarray(xvalueHist, dtype=np.float64)
+np.asarray(tskyHist, dtype=np.float64)
+np.asarray(sd)
+
 f = plt.figure()
 plotNum = 1
 for k in range(3):
@@ -92,5 +106,50 @@ for k in range(3):
 	plt.xlabel("Frequency")
 	plt.ylabel("Tsky")
 	plt.ylim((-100, 150))	
+	plotNum = plotNum + 1
+plt.show()
+
+pwd = "/home/gurbir/kek/water-vapour-cmb/am/"
+def pwv2trop(pwv):
+	return (float(pwv)-0.0029)/1.8502
+
+def residual(p, x, data, sd):
+	pwv = p['pwv']
+	trop_h20_scale = pwv2trop(pwv)
+	call("am " + pwd + "northern_midlatitude_MAM.amc 18198349174.587299 Hz" + 
+		" 26602551275.637801 Hz 200100050.025 Hz 0 deg " +  str(trop_h20_scale) +
+		" > amModel.out", shell=True)
+	f = open('amModel.out', 'r')
+	y = []
+	for line in f:
+		words = line.split(' ')
+		y.append(words[1])
+	y = np.asarray(y, dtype=np.float64)
+	return np.divide(np.subtract(y, data), sd)
+
+final = []
+bestfit = []
+for k in range(3):
+	params = Parameters()
+	params.add('pwv', value=2., min=0.1, max=10)
+
+	minner = Minimizer(residual, params, fcn_args=(xvalueHist, tskyHist[k], sd[k]))
+	result = minner.minimize(method='nelder')
+	bestfit.append(result.params['pwv'].value)
+
+	final.append(tskyHist[k] + np.multiply(result.residual, sd[k]))
+
+	report_fit(result)
+
+f = plt.figure()
+plotNum = 1
+for k in range(3):
+	ax = f.add_subplot(2, 2, plotNum)
+	plt.errorbar(xvalueHist, tskyHist[k], yerr=sd[k], fmt='o')
+	plt.plot(xvalueHist, final[k])
+	ax.text(0.4, 0.1, str(bestfit[k]) + ' mm PWV', transform=ax.transAxes)
+	plt.title("Observation " + str(k+1))
+	plt.xlabel("Frequency")
+	plt.ylabel("Tsky")
 	plotNum = plotNum + 1
 plt.show()
